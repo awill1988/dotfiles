68bd722bdb2fd888a6f810443d6fcd0f4359ecc2
Adam Williams <adam@williams.engineer>
2025-12-18 14:53:58 -0700

    fix: disable bufferline due to compatibility issues
    temporarily disabled until configuration can be fixed

--- home/config/nvim/lua/plugins/bufferline.lua
+++ home/config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,3 @@
 -- See current buffers at the top of the editor
-return {
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {},
-	},
-}
+-- Disabled due to compatibility issues
+return {}
